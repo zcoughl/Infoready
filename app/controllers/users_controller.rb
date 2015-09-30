09f019eac21adb412d42d33e601421f2e1e23ed3
JingzhuY <yancarol@umich.edu>
2015-09-30 10:51:05 -0400

    with profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,11 +22,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+  end
+
   private
 
   	def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925214743) do
+ActiveRecord::Schema.define(version: 20150930035913) do
 
   create_table "entries", force: :cascade do |t|
     t.integer  "field_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150925214743) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "company"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
