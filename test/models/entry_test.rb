99490c109a2e022b05da9c9ed8ac20196915c4a1
shenghua <shenghua@0587379561.wireless.umich.net>
2015-11-04 13:06:28 -0500

    test records

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,9 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "column name should not be too long" do
+	@entry.colname = "a" * 51
+	assert_not @entry.valid?
+	asser true
+  end
 end
